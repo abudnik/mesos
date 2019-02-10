8f7eab0d9a0b47de7015b3d52dacb712cda5b29a
Jie Yu <yujie.jay@gmail.com>
2019-02-09 22:51:16 -0800

    Secured mesos executor binary using memfd.

--- src/slave/containerizer/mesos/launch.cpp
+++ src/slave/containerizer/mesos/launch.cpp
@@ -869,9 +869,16 @@ int MesosContainerizerLaunch::execute()
 
     // Avoid leaking not required file descriptors into the forked process.
     foreach (int_fd fd, fds.get()) {
-      // We use the unwrapped `::close` as opposed to `os::close`
-      // since the former is guaranteed to be async signal safe.
-      ::close(fd);
+      // NOTE: Set "FD_CLOEXEC" on the fd, instead of closing it
+      // because exec below might exec a memfd.
+      int flags = ::fcntl(fd, F_GETFD);
+      if (flags == -1) {
+        cerr << "Failed to get FD flags";
+        os::close(fd);
+      } else if (::fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1) {
+        cerr << "Failed to set FD_CLOEXEC";
+        os::close(fd);
+      }
     }
   }
 #endif // __WINDOWS__
